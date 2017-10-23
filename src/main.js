bebc61b93a3b0aee68040e7c9ed9bc2712cdeafa
sulenchy <sulaimanabiodun172@gmail.com>
2017-10-23 20:54:22 +0100

    committing cleaner code

--- src/main.js
+++ src/main.js
@@ -1,48 +1,54 @@
-'use strict'
-//exports the rainDropSpeak function
-module.exports={
-    
-    rainDropSpeak: (value) => {
-        if(typeof(value) != 'number'){
-            return 'undefined';
-        }else if(value < 0){
-            return 'undefined';
-        }else{
-            var result =[];
-            var answer=[];
-            for (let i=1; i < value; i++){
-              var count = 0
-              for (let j=1; j<=i; j++){
-                if(i%j == 0){
-                  count++;
-                }
-              }
-              if (count == 2){
-                result.push(i) ;
-              }
-            }
-            for(var i=0; i<result.length; i++){
-              if(value%result[i] == 0){
-                answer.push(result[i]);
-              }
-              
-            }
-            var res = "";
-            for (var i=0; i< answer.length; i++ ){
-              if(answer[i]== 3){
-                res=res+"Pling";
-              }else if(answer[i]== 5){
-                res=res+"Plang";
-              }else if(answer[i]== 7){
-                res=res+"Plong";
-              }
-            }
-            if(res == ""){
-              return value;
-            }else{
-              return res;
-            }
-        }
-        
+'use script';
+
+// exports the rainDropSpeak function
+module.exports = {
+  // function 'rainDropSpeak' with argument 'value'
+  rainDropSpeak: (value) => {
+    // this block test whether the value passed is a number or not
+    if (typeof (value) !== 'number') {
+      return 'undefined';
     }
-}
+    // test whether the value entered is less than zero or not
+    else if (value <= 0) {
+      return 'undefined';
+    }
+
+    // empty list of prime numbers,
+    const primeNumberArr = [];
+    const primeFactorArr = [];
+    for (let i = 1; i < value; i += 1) {
+      let count = 0;
+      for (let j = 1; j <= i; j += 1) {
+        if (i % j === 0) {
+          count += 1;
+        }
+      }
+      if (count === 2) {
+        primeNumberArr.push(i);
+      }
+    }
+    for (var i = 0; i < primeNumberArr.length; i++) {
+      if (value % primeNumberArr[i] == 0) {
+        primeFactorArr.push(primeNumberArr[i]);
+      }
+
+    }
+    let res = '';
+    for (var i = 0; i < primeFactorArr.length; i++) {
+      if (primeFactorArr[i] == 3) {
+        res += 'Pling';
+      } else if (primeFactorArr[i] == 5) {
+        res += 'Plang';
+      } else if (primeFactorArr[i] == 7) {
+        res += 'Plong';
+      }
+    }
+    if (res == '') {
+      return value;
+    }
+    return res;
+
+
+
+  }
+};
--- tests/test.js
+++ tests/test.js
@@ -1,41 +1,47 @@
 const assert = require('chai').assert;
 const myApp = require('../src/main.js');
 
-describe("RainDropSpeak", function(){
+describe('RainDropSpeak', () => {
 
-describe("Handle valid input",function(){
-    it("should return 'Plong' as input for 28", function(){
-    assert.equal(myApp.rainDropSpeak(28),"Plong");
+  describe('Handle valid input', () => {
+    it("should return 'Plong' as input for 28", () => {
+      assert.equal(myApp.rainDropSpeak(28), 'Plong');
     });
-    it("should return 'Pling' as input for 9", function(){
-        assert.equal(myApp.rainDropSpeak(9),"Pling");
+    it("should return 'Pling' as input for 9", () => {
+      assert.equal(myApp.rainDropSpeak(9), 'Pling');
     });
-    it("should return 'Pling' as input for 11", function(){
-        assert.equal(myApp.rainDropSpeak(11),"11");
+    it("should return 'Pling' as input for 11", () => {
+      assert.equal(myApp.rainDropSpeak(11), '11');
     });
-    it("should return '34' as input for 34", function(){
-        assert.equal(myApp.rainDropSpeak(34),"34");
+    it("should return 'Pling' as input for 1001", () => {
+        assert.equal(myApp.rainDropSpeak(1001), 'Plong');
+      });
+    it("should return '34' as input for 34", () => {
+      assert.equal(myApp.rainDropSpeak(34), '34');
     });
-    it("should return 'PlingPlang' as input for 1755", function(){
-        assert.equal(myApp.rainDropSpeak(1755),"PlingPlang");
+    it("should return 'PlingPlang' as input for 1755", () => {
+      assert.equal(myApp.rainDropSpeak(1755), 'PlingPlang');
     });
- 
+
+  });
+
+
+  describe('Handle invalid input', () => {
+
+    it('should return undefined as input for -3', () => {
+      assert.equal(myApp.rainDropSpeak(-3), 'undefined');
+    });
+    it('should return undefined as input for 0', () => {
+        assert.equal(myApp.rainDropSpeak(0), 'undefined');
+      });
+
+    it("should return undefined as input for '-3'", () => {
+      assert.equal(myApp.rainDropSpeak('-3'), 'undefined');
+    });
+
+    it("should return undefined as input for 'demo'", () => {
+      assert.equal(myApp.rainDropSpeak('demo'), 'undefined');
+    });
+  });
+
 });
-
-
-describe("Handle invalid input",function(){
- 
- it("should return undefined as input for -3", function(){
-  assert.equal(myApp.rainDropSpeak(-3),'undefined');
- });
-
- it("should return undefined as input for '-3'", function(){
-    assert.equal(myApp.rainDropSpeak('-3'),'undefined');
-   });
- 
- it("should return undefined as input for 'demo'", function(){
-    assert.equal(myApp.rainDropSpeak('demo'),'undefined');
-   });
-});
-
-})
